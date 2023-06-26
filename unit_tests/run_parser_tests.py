068117352afd3a43bed4686bed98bba9d961292e
pc852 <pengchen_twd@yahoo.com.sg>
2023-06-26 18:06:55 +0200

    Update run_parser_tests.py
    removed all newlines and spacing when checking output

--- unit_tests/run_parser_tests.py
+++ unit_tests/run_parser_tests.py
@@ -9,17 +9,19 @@ test4 = 'parser_test_4.lp'
 test5 = 'parser_test_5.lp'
 test6 = 'parser_test_6.lp'
 
-answer1 = "Learned preference statement: \n \n#preference(test1,type){\n  atom(a(1)) \n}.\n"
-answer2 = "Learned preference statement: \n \n#preference(test2,type1){\n  atom(a(1)) ; \n  atom(a(2)) ; \n  atom(a(3)) \n}.\n"
-answer3 = "Learned preference statement: \n \n#preference(test3a,type1){\n  atom(a(1)) ; \n  atom(a(2)) \n}.\n\nLearned preference statement: \n \n#preference(test3b,type2){\n  atom(b(1)) ; \n  atom(b(2)) ; \n  atom(b(3)) \n}.\n"
-answer4 = "Learned preference statement: \n \n#preference(test4a,type1){\n  5 :: atom(a(1)) ; \n  10 :: atom(a(2)) \n}.\n\nLearned preference statement: \n \n#preference(test4b,type2){\n  3,4 :: atom(b(1)) ; \n  5,6 :: atom(b(2)) ; \n  1,2 :: atom(b(3)) ; \n  200 :: atom(bb(b(2))) >> 300 :: atom(bb(b(3))) || 100 :: atom(bb(b(1))) \n}.\n"
-answer5 = "Learned preference statement: \n \n#preference(test5a,type1){\n  5 :: atom(a(1)) & not atom(a(2))  ; \n  5 :: not atom(a(2))  | (not atom(a(3))  & (atom(a(4)) | not atom(a(5)) )) \n}.\n\nLearned preference statement: \n \n#preference(test5b,type2){\n  5,6 :: not atom(b(3))  & not atom(b(4))  >> 1,2 :: atom(b(5)) | not atom(b(6))  || 3,4 :: atom(b(1)) | atom(b(2)) \n}.\n"
-answer6 = "Learned preference statement: \n \n#preference(test6a,type1){\n  100 :: **apple || 50 :: **banana \n}.\n\nLearned preference statement: \n \n#preference(test6b,type1){\n  **apple ; \n  200 :: **coconut >> 300 :: **durian || 100 :: **banana \n}.\n"
+answer1 = "Learnedpreferencestatement:#preference(test1,type){atom(a(1))}."
+answer2 = "Learnedpreferencestatement:#preference(test2,type1){atom(a(1));atom(a(2));atom(a(3))}."
+answer3 = "Learnedpreferencestatement:#preference(test3a,type1){atom(a(1));atom(a(2))}.Learnedpreferencestatement:#preference(test3b,type2){atom(b(1));atom(b(2));atom(b(3))}."
+answer4 = "Learnedpreferencestatement:#preference(test4a,type1){5::atom(a(1));10::atom(a(2))}.Learnedpreferencestatement:#preference(test4b,type2){3,4::atom(b(1));5,6::atom(b(2));1,2::atom(b(3));200::atom(bb(b(2)))>>300::atom(bb(b(3)))||100::atom(bb(b(1)))}."
+answer5 = "Learnedpreferencestatement:#preference(test5a,type1){5::atom(a(1))&notatom(a(2));5::notatom(a(2))|(notatom(a(3))&(atom(a(4))|notatom(a(5))))}.Learnedpreferencestatement:#preference(test5b,type2){5,6::notatom(b(3))&notatom(b(4))>>1,2::atom(b(5))|notatom(b(6))||3,4::atom(b(1))|atom(b(2))}."
+answer6 = "Learnedpreferencestatement:#preference(test6a,type1){100::**apple||50::**banana}.Learnedpreferencestatement:#preference(test6b,type1){**apple;200::**coconut>>300::**durian||100::**banana}."
 
 def run_test(number, test, answer):
     result = subprocess.run(['python3', learning, '-W', 'none', backend, test], stdout=subprocess.PIPE)
     result = result.stdout.decode('utf-8')
     result = result[result.find('Learned preference statement'):]
+    result = result.replace(" ","")
+    result = result.replace("\n","")
     try:
         assert result == answer
         print("test " + str(number) + " passed.")
