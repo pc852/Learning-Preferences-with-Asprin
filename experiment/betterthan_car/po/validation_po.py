d302d06b2a5891560c3795842968b8f43c3c978a
pc852 <pengchen_twd@yahoo.com.sg>
2023-12-19 20:37:40 +0800

    added new experiments sets
    (recommit) 10f and min10f with expanded user list

---
+++ experiment/betterthan_car/po/validation_po.py
@@ -0,0 +1,47 @@
+import sys
+import clingo
+from clingo.application import clingo_main, Application
+from clingo.script import enable_python
+from clingo.symbol import String, Number
+from clingo.control import Control
+import subprocess
+import random
+import shutil
+import os
+
+
+users = [4,7,11,15,17,19,38,39,42,43,51,52,55] #perfert users are: 4,7,11,15,17,19,38,39,42,43,51,52,55
+val_set_1 = '../dataset_bt10fold_minimized/val1/validation_po/user'
+output_1 = '../dataset_bt10fold_minimized/training_po_v1/user'
+gen_types = ["bt"]
+#gen_types = ["poset"]
+#"aso","less_weight"],
+val_set_2 = '/validation_set_.lp'
+output_2 = '/training_set.lp/run1/learned_preference_instances.lp'
+        
+for u in users:
+    for g in gen_types:
+        file1 = val_set_1+str(u)+'/'+g+val_set_2
+        file2 = output_1+str(u)+'/'+g+output_2
+
+        result = subprocess.run(['./../../../asprin-vL', '--min_element', '-W', 'none', file1, file2], stdout=subprocess.PIPE)
+        stdout = result.stdout.decode('utf-8')
+        stdout_show = result.stdout.decode('utf-8')
+        
+        print('\n user :',u , ' type: ', g)
+        nPerfect = stdout.rfind('Optimum')
+        if nPerfect != -1:
+            stdout = stdout[stdout.rfind('Optimum'):]
+            #stdout = stdout[:stdout.rfind('Calls')]
+        else:
+            stdout = stdout[stdout.rfind('SATISFIABLE'):]
+            #stdout = stdout[:stdout.rfind('Models')]
+        print(stdout)
+
+
+            
+            
+            
+            
+
+
